3ea34bc824174ce9d5c6c92cc8c39e9e538e4872
vasanth <v.g.vasanthakumar@gmail.com>
2020-04-10 13:26:02 +0530

    login

---
+++ lib/AuthenticationScreens/login_page.dart
@@ -0,0 +1,208 @@
+import 'dart:async';
+
+import 'package:tellthetruth/AuthenticationScreens/signup_page.dart';
+import 'package:tellthetruth/AuthenticationScreens/email_authentaction_page.dart';
+import 'package:tellthetruth/common_variables/app_colors.dart';
+import 'package:tellthetruth/common_variables/app_fonts.dart';
+import 'package:tellthetruth/common_widgets/button_widget/to_do_button.dart';
+import 'package:flutter/material.dart';
+import 'package:tellthetruth/firebase/auth.dart';
+import 'package:provider/provider.dart';
+import '../widgets/slide_item.dart';
+import '../model/slide.dart';
+import '../widgets/slide_dots.dart';
+import 'login_page_manager.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage(
+      {Key key, @required this.manager, @required this.isLoading})
+      : super(key: key);
+  final LoginPageManager manager;
+  final bool isLoading;
+
+  static Widget create(BuildContext context) {
+    final auth = Provider.of<AuthBase>(context, listen: false);
+    return ChangeNotifierProvider<ValueNotifier<bool>>(
+      create: (_) => ValueNotifier<bool>(false),
+      child: Consumer<ValueNotifier<bool>>(
+        builder: (_, isLoading, __) =>
+            Provider<LoginPageManager>(
+              create: (_) =>
+                  LoginPageManager(auth: auth, isLoading: isLoading),
+              child: Consumer<LoginPageManager>(
+                builder: (context, manager, _) =>
+                    LoginPage(
+                      manager: manager, isLoading: isLoading.value,
+                    ),
+              ),
+            ),
+      ),
+    );
+  }
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  int _currentPage = 0;
+  final PageController _pageController = PageController(initialPage: 0);
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 5), (Timer timer) {
+      if (_currentPage < 2) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+
+      _pageController.animateToPage(
+        _currentPage,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  _onPageChanged(int index) {
+    setState(() {
+      _currentPage = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Stack(
+                  alignment: AlignmentDirectional.bottomCenter,
+                  children: <Widget>[
+                    PageView.builder(
+                      scrollDirection: Axis.horizontal,
+                      controller: _pageController,
+                      onPageChanged: _onPageChanged,
+                      itemCount: slideList.length,
+                      itemBuilder: (ctx, i) => SlideItem(i),
+                    ),
+                    Stack(
+                      alignment: AlignmentDirectional.topStart,
+                      children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 35),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              for(int i = 0; i<slideList.length; i++)
+                                if( i == _currentPage )
+                                  SlideDots(true)
+                                else
+                                  SlideDots(false)
+                            ],
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(left:20.0,right: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      height: 50.0,
+                      width: 400,
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => SignupPage(),),
+                          );
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: backgroundColor,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Center(
+                                child: Text(
+                                  "Sign Up",
+                                  style: subTitleDark,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Container(
+                      height: 50.0,
+                      width: 400,
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => EmailAuthenticationPage(),),
+                          );
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: backgroundColor,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Center(
+                                child: Text(
+                                  "Log In",
+                                  style: subTitleDark,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+}
--- lib/common_variables/app_fonts.dart
+++ lib/common_variables/app_fonts.dart
@@ -7,43 +7,43 @@ var titleLight = TextStyle(
     color: Color(0xFF253949),
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w900,
-    fontSize: 50.0,decoration: TextDecoration.none);
+    fontSize: 30.0,decoration: TextDecoration.none);
 var subTitleLight = TextStyle(
     color: Color(0xFF253949),
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w900,
-    fontSize: 50.0,decoration: TextDecoration.none);
+    fontSize: 25.0,decoration: TextDecoration.none);
 var contentLight = TextStyle(
     color: Color(0xFF253949),
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w900,
-    fontSize: 50.0,decoration: TextDecoration.none);
+    fontSize: 20.0,decoration: TextDecoration.none);
 var descriptionLight = TextStyle(
     color: Color(0xFF253949),
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w900,
-    fontSize: 50.0,decoration: TextDecoration.none);
+    fontSize: 16.0,decoration: TextDecoration.none);
 
 var titleDark = TextStyle(
     color: Color(0xFF253949),
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w900,
-    fontSize: 50.0,decoration: TextDecoration.none);
+    fontSize: 30.0,decoration: TextDecoration.none);
 var subTitleDark = TextStyle(
     color: Color(0xFF253949),
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w900,
-    fontSize: 50.0,decoration: TextDecoration.none);
+    fontSize: 25.0,decoration: TextDecoration.none);
 var contentDark = TextStyle(
     color: Color(0xFF253949),
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w900,
-    fontSize: 50.0,decoration: TextDecoration.none);
+    fontSize: 20.0,decoration: TextDecoration.none);
 var descriptionDark = TextStyle(
     color: Color(0xFF253949),
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w900,
-    fontSize: 50.0,decoration: TextDecoration.none);
+    fontSize: 16.0,decoration: TextDecoration.none);
 
 
 

bd535b0337a027b26f6a475e73382d839713573f
vasanth <v.g.vasanthakumar@gmail.com>
2020-04-14 21:05:23 +0530

    dashboard

--- lib/home/AddItem.dart
+++ lib/home/AddItem.dart
@@ -3,13 +3,17 @@ import 'dart:typed_data';
 
 import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_page_transition/flutter_page_transition.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:interactive_add_button_layout/interactive_add_button_layout.dart';
 import 'package:screenshot/screenshot.dart';
 import 'package:swipe_up_menu/swipe_up_menu.dart';
 import 'package:tellthetruth/common_variables/app_colors.dart';
 import 'package:tellthetruth/common_variables/app_fonts.dart';
 import 'package:tellthetruth/common_widgets/custom_appbar_widget/custom_app_bar.dart';
 import 'package:tellthetruth/common_widgets/offline_widgets/offline_widget.dart';
+import 'package:tellthetruth/home/AddGroup.dart';
+import 'package:tellthetruth/home/groupMembers.dart';
 
 class AddItemPage extends StatelessWidget {
   //ProfilePage({@required this.database});
@@ -64,108 +68,168 @@ class _F_AddItemPageState extends State<F_AddItemPage> {
               Color(0Xff12c2e9),
             ],
           )),
-      child: Scaffold(
-        body: Container(
-          color: const Color(0xFF192A56),
-          child: Center(
-            child: RaisedButton(
-              onPressed: () {
-                // The menu can be handled programatically using a key
-                if (fabKey.currentState.isOpen) {
-                  fabKey.currentState.close();
-                } else {
-                  fabKey.currentState.open();
-                }
-              },
-              color: Colors.white,
-              child: Text('Toggle menu programatically', style: TextStyle(color: primaryColor)),
-            ),
+      child: AddButtonLayout(
+        color: Colors.white,
+        btnColor: Colors.black,
+        child: Padding(
+          padding: EdgeInsets.only(top: 80, left: 10),
+          child: Column(
+            children: <Widget>[
+              myWidget(),
+              myWidget(),
+              myWidget(),
+              myWidget(),
+            ],
           ),
         ),
-          floatingActionButton: Builder(
-            builder: (context) => FabCircularMenu(
-              key: fabKey,
-              // Cannot be `Alignment.center`
-              alignment: Alignment.bottomRight,
-              ringColor: Colors.white.withAlpha(25),
-              ringDiameter: 800.0,
-              ringWidth: 250.0,
-              fabSize: 84.0,
-              fabElevation: 8.0,
-
-              // Also can use specific color based on wether
-              // the menu is open or not:
-              // fabOpenColor: Colors.white
-              // fabCloseColor: Colors.white
-              // These properties take precedence over fabColor
-              fabColor: Colors.white,
-              fabOpenIcon: Icon(Icons.menu, color: primaryColor),
-              fabCloseIcon: Icon(Icons.close, color: primaryColor),
-              fabMargin: const EdgeInsets.all(16.0),
-              animationDuration: const Duration(milliseconds: 800),
-              animationCurve: Curves.easeInOutCirc,
-              onDisplayChange: (isOpen) {
-                _showSnackBar(context, "The menu is ${isOpen ? "open" : "closed"}");
-              },
-              children: <Widget>[
-                RawMaterialButton(
-                  onPressed: () {
-                    _showSnackBar(context, "You pressed 1");
-                  },
-                  shape: CircleBorder(),
-                  padding: const EdgeInsets.all(24.0),
-                  child: Icon(Icons.looks_one, color: Colors.white),
+        row: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 150,),
+          ),
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context, PageTransition(type: PageTransitionType.rippleRightDown, duration: Duration(seconds: 1), child: GangName()));
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 110,
+                height: 130,
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Image( image: AssetImage("images/male.png"),height: 55,width: 55,),
+                      Text("Add Group",textAlign: TextAlign.center,style: regularStyle,),
+                      ],
+                  ),
                 ),
-                RawMaterialButton(
-                  onPressed: () {
-                    _showSnackBar(context, "You pressed 2");
-                  },
-                  shape: CircleBorder(),
-                  padding: const EdgeInsets.all(24.0),
-                  child: Icon(Icons.looks_two, color: Colors.white),
+              ),
+            ),
+          ),
+          Padding(
+              padding: EdgeInsets.only(top: 10, bottom: 10,),
+              child: Container(
+                width: 110,
+                height: 130,
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Image( image: AssetImage("images/female.png"),height: 55,width: 55,),
+                      Text("Add Question",textAlign: TextAlign.center,style: regularStyle,),
+                    ],
+                  ),
                 ),
-                RawMaterialButton(
-                  onPressed: () {
-                    _showSnackBar(context, "You pressed 3");
-                  },
-                  shape: CircleBorder(),
-                  padding: const EdgeInsets.all(24.0),
-                  child: Icon(Icons.looks_3, color: Colors.white),
+              )),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: 110,
+              height: 130,
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white),
+              child: Container(
+                child: Column(
+                  children: [
+                    Image( image: AssetImage("images/girl.png"),height: 55,width: 55,),
+                    Text("Add Group",textAlign: TextAlign.center,style: regularStyle,),
+                  ],
                 ),
-                RawMaterialButton(
-                  onPressed: () {
-                    _showSnackBar(context, "You pressed 4. This one closes the menu on tap");
-                    fabKey.currentState.close();
-                  },
-                  shape: CircleBorder(),
-                  padding: const EdgeInsets.all(24.0),
-                  child: Icon(Icons.looks_4, color: Colors.white),
-                ),
-                RawMaterialButton(
-                  onPressed: () {
-                    _showSnackBar(context, "You pressed 4. This one closes the menu on tap");
-                    fabKey.currentState.close();
-                  },
-                  shape: CircleBorder(),
-                  padding: const EdgeInsets.all(24.0),
-                  child: Icon(Icons.looks_4, color: Colors.white),
-                )
-              ],
+              ),
             ),
           )
-      )
-
-
-    );
+        ],
+        column: <Widget>[
+        ],
+      ));
   }
-  void _showSnackBar (BuildContext context, String message) {
-    Scaffold.of(context).showSnackBar(
-        SnackBar(
-          content: Text(message),
-          duration: const Duration(milliseconds: 1000),
+
+  Widget myWidget() {
+    return Container(
+        height: 120,
+        width: 350,
+        padding: EdgeInsets.all(5),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(2, 2, 0, 0),
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 30, left: 10),
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Color(0xffEBC0D2).withOpacity(0.5),
+                      shape: BoxShape.circle),
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(left: 10, top: 10),
+                  child: Container(
+                    height: 150,
+                    width: 250,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xffEBC0D2).withOpacity(0.2)),
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(top: 10, right: 10),
+                          child: Container(
+                            width: 200,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(60),
+                              color: Color(0xfdedede).withOpacity(0.4),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 10,
+                          ),
+                          child: Container(
+                            width: 220,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(60),
+                              color: Color(0xfdedede).withOpacity(0.3),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 10,
+                          ),
+                          child: Container(
+                            width: 220,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(60),
+                              color: Color(0xfdedede).withOpacity(0.3),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ))
+            ],
+          ),
         )
+
+
     );
   }
+
 }
 
 

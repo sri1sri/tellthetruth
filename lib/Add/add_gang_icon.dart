148131ca821ed5bebebd9d5edd848ee3ad42c236
sri209 <kondasri98@yahoo.com>
2020-04-15 17:58:50 +0530

    size issue

--- lib/Add/add_gang_icon.dart
+++ lib/Add/add_gang_icon.dart
@@ -111,7 +111,7 @@ class _F_AddGangIconState extends State<F_AddGangIcon> {
                         child: Container(
                           color: Colors.transparent,
                           child: Padding(
-                            padding: const EdgeInsets.fromLTRB(20, 50, 20, 40),
+                            padding: const EdgeInsets.fromLTRB(10, 50, 10, 40),
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               crossAxisAlignment: CrossAxisAlignment.start,
@@ -148,28 +148,28 @@ class _F_AddGangIconState extends State<F_AddGangIcon> {
                                         alignment: AlignmentDirectional.topStart,
                                         isRepeatingAnimation: false,// or Alignment.topLeft
                                       ),
-                                      SizedBox(
-                                        height: getDynamicHeight(20),
-                                      ),
-                                      GestureDetector(
-                                        child: Container(
-                                          width: getDynamicWidth(150.0),
-                                          padding: EdgeInsets.all(15.0),
-                                          child: Center(
-                                              child: Lottie.network('https://assets7.lottiefiles.com/packages/lf20_O2YdXL.json',height: getDynamicHeight(100),width: getDynamicWidth(100)),
-                                          ),
-                                          decoration: BoxDecoration(
-                                              borderRadius: BorderRadius.circular(15.0), ),
-                                        ),
-                                        onTap: () {
-//                                      // The menu can be handled programatically using a key
-                                          if (fabKey.currentState.isOpen) {
-                                            fabKey.currentState.close();
-                                          } else {
-                                            fabKey.currentState.open();
-                                          }
-                                        },
-                                      ),
+//                                      SizedBox(
+//                                        height: getDynamicHeight(20),
+//                                      ),
+//                                      GestureDetector(
+//                                        child: Container(
+//                                          width: getDynamicWidth(150.0),
+//                                          padding: EdgeInsets.all(15.0),
+//                                          child: Center(
+//                                              child: Lottie.network('https://assets7.lottiefiles.com/packages/lf20_O2YdXL.json',height: getDynamicHeight(100),width: getDynamicWidth(100)),
+//                                          ),
+//                                          decoration: BoxDecoration(
+//                                              borderRadius: BorderRadius.circular(15.0), ),
+//                                        ),
+//                                        onTap: () {
+////                                      // The menu can be handled programatically using a key
+//                                          if (fabKey.currentState.isOpen) {
+//                                            fabKey.currentState.close();
+//                                          } else {
+//                                            fabKey.currentState.open();
+//                                          }
+//                                        },
+//                                      ),
                                     ],
                                   ),
                                 ),
@@ -220,6 +220,7 @@ class _F_AddGangIconState extends State<F_AddGangIcon> {
                                                 ])),
                                         decoration: BoxDecoration(
                                             color: Colors.white.withOpacity(0.7),
+
 //                            gradient: LinearGradient(
 //                                colors: <Color>[
 //                                Color(0XffFD8B1F),
@@ -227,7 +228,7 @@ class _F_AddGangIconState extends State<F_AddGangIcon> {
 //                            Color(0Xff30D0DB),
 //                            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
 
-                                            borderRadius: BorderRadius.circular(15.0),
+                                            borderRadius: BorderRadius.circular(30.0),
                                             boxShadow: [
                                               BoxShadow(
                                                   color: Colors.grey,
@@ -263,10 +264,9 @@ class _F_AddGangIconState extends State<F_AddGangIcon> {
   Widget icon(String imgpath )
   {
     return Container(
-      color: Colors.deepPurple,
       child: Center(
         child: Card(
-          child:Lottie.network(imgpath,height: getDynamicHeight(100),width: getDynamicWidth(100)),
+          child:Lottie.network(imgpath,height: getDynamicHeight(200),width: getDynamicWidth(200)),
         ),
       ),
     );
@@ -275,18 +275,19 @@ class _F_AddGangIconState extends State<F_AddGangIcon> {
 
   Widget getVariableScaleCrousel() {
     return FinitePager(
+      onPageChanged: ,
       scaleX: 0.8,
       scaleY: 0.4,
       scrollDirection: Axis.horizontal,
       children: <Widget>[
         icon("https://assets7.lottiefiles.com/packages/lf20_O2YdXL.json"),
-        icon("https://assets7.lottiefiles.com/packages/lf20_O2YdXL.json"),
-        icon("https://assets7.lottiefiles.com/packages/lf20_O2YdXL.json"),
-        icon("https://assets7.lottiefiles.com/packages/lf20_O2YdXL.json"),
-        icon("https://assets7.lottiefiles.com/packages/lf20_O2YdXL.json"),
-        icon("https://assets7.lottiefiles.com/packages/lf20_O2YdXL.json"),
-        icon("https://assets7.lottiefiles.com/packages/lf20_O2YdXL.json"),
-        icon("https://assets7.lottiefiles.com/packages/lf20_O2YdXL.json"),
+        icon("https://assets7.lottiefiles.com/packages/lf20_VCStus.json"),
+        icon("https://assets7.lottiefiles.com/packages/lf20_uwmgvS.json"),
+        icon("https://assets7.lottiefiles.com/packages/lf20_CFgBAP.json"),
+        icon("https://assets7.lottiefiles.com/packages/lf20_OyFTHm.json"),
+        icon("https://assets7.lottiefiles.com/packages/lf20_BonJMC.json"),
+        icon("https://assets7.lottiefiles.com/packages/lf20_RWZde1.json"),
+        icon("https://assets7.lottiefiles.com/packages/lf20_KMustJ.json"),
       ],
     );
   }

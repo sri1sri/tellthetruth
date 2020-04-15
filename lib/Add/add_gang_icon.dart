088ff4a911d812b9e0557c491ecaad5f8f20bca6
vasanth <v.g.vasanthakumar@gmail.com>
2020-04-15 20:15:50 +0530

    text animation

--- lib/Add/add_gang_icon.dart
+++ lib/Add/add_gang_icon.dart
@@ -1,6 +1,10 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:finite_coverflow/finite_coverflow.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter_page_transition/flutter_page_transition.dart';
+import 'package:tellthetruth/home/dashboard.dart';
+import 'package:vector_math/vector_math.dart' as math;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_text/gradient_text.dart';
@@ -163,11 +167,8 @@ class _F_AddGangIconState extends State<F_AddGangIcon> {
 
 
                                 Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  mainAxisAlignment: MainAxisAlignment.center,
                                   children: <Widget>[
-                                    Container(
-                                      child: Text(""),
-                                    ),
                                     GestureDetector(
                                       child: Container(
                                         width: getDynamicWidth(200.0),
@@ -179,7 +180,7 @@ class _F_AddGangIconState extends State<F_AddGangIcon> {
                                                 children: <Widget>[
                                                   Container(),
                                                   GradientText(
-                                                    'Continue',
+                                                    'Create',
                                                     style: boldStyle,
                                                     gradient: LinearGradient(
                                                       colors: [
@@ -190,11 +191,6 @@ class _F_AddGangIconState extends State<F_AddGangIcon> {
                                                       begin: Alignment.topLeft,
                                                       end: Alignment.bottomRight,
                                                     ),
-                                                  ),
-                                                  Icon(
-                                                    Icons.arrow_forward_ios,
-                                                    color: Colors.blue,
-                                                    size: getDynamicTextSize(30),
                                                   ),
                                                   Container(),
                                                 ])),
@@ -207,7 +203,7 @@ class _F_AddGangIconState extends State<F_AddGangIcon> {
 //                            Color(0Xff30D0DB),
 //                            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
 
-                                            borderRadius: BorderRadius.circular(15.0),
+                                            borderRadius: BorderRadius.circular(30.0),
                                             boxShadow: [
                                               BoxShadow(
                                                   color: Colors.grey,
@@ -217,11 +213,7 @@ class _F_AddGangIconState extends State<F_AddGangIcon> {
                                             ]),
                                       ),
                                       onTap: () {
-//                                      setState(() {
-//                                        generateGroupID = groupID.groupID+1;
-//                                      });
-//                                      _submit();
-                                        GoToPage(context, AddGangIcon());
+                                        showFancyCustomDialog( context );
                                       },
                                     ),
                                   ],
@@ -277,4 +269,160 @@ class _F_AddGangIconState extends State<F_AddGangIcon> {
   }
 
 
+}
+
+
+void showFancyCustomDialog(BuildContext context) {
+
+  showGeneralDialog(
+      context: context,
+      pageBuilder: (context, anim1, anim2) {},
+      barrierDismissible: true,
+      barrierColor: Colors.black.withOpacity(0.4),
+      barrierLabel: '',
+      transitionBuilder: (context, anim1, anim2, child) {
+        return Transform.rotate(
+          angle: math.radians(anim1.value * 360),
+          child: Opacity(
+            opacity: anim1.value,
+            child: Dialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                height: getDynamicHeight(600.0),
+                width: getDynamicWidth(320.0),
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      width: double.infinity,
+                      height: getDynamicHeight(600),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(height: getDynamicHeight(60),),
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Center(
+                                    child: SizedBox(
+                                      width: getDynamicWidth(200),
+                                      height: getDynamicHeight(200),
+                                      child: Container(
+                                          child: FlareActor("images/success-2.flr",
+                                              alignment: Alignment.topRight,
+                                              fit: BoxFit.contain,
+                                              animation: 'check-success')),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 15),
+                              child: Divider(color: Colors.black54,thickness: 1,),
+                            ),
+                            Column(
+                              children: [
+                                Text("2DS6AHA",style: boldStyle,),
+                                 Lottie.network("https://assets7.lottiefiles.com/packages/lf20_O2YdXL.json",height: getDynamicHeight(100),width: getDynamicWidth(100)),
+                                Text("ROCKSTARS",style: boldStyle,)
+                              ],
+                            ),
+                            SizedBox(
+                              height: getDynamicHeight(20),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 15),
+                              child: Text("Note : The group code has been sent to your group members, then can access the group by it.",style: regularStyle,),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: getDynamicHeight(50),
+                      alignment: Alignment.bottomCenter,
+                      decoration: BoxDecoration(
+                        color: Colors.greenAccent,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12),
+                        ),
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                            "Success",
+                            style: questionStyle
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(context, PageTransition(type: PageTransitionType.rippleLeftUp, duration: Duration(seconds: 1), child: DashboardPage()));
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          height: getDynamicHeight(50),
+                          decoration: BoxDecoration(
+                            color: Colors.blue[300],
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(12),
+                              bottomRight: Radius.circular(12),
+                            ),
+                          ),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              "Okay let's go!",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      // These values are based on trial & error method
+                      alignment: Alignment(1.05, -1.05),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Icon(
+                            Icons.close,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+      transitionDuration: Duration(milliseconds: 300));
 }

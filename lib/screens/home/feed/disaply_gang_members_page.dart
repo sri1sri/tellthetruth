dda9120eb70ce0b2bf5b5d34414fbed81a390196
vasanth <v.g.vasanthakumar@gmail.com>
2020-04-17 22:32:59 +0530

    class

--- lib/screens/home/feed/disaply_gang_members_page.dart
+++ lib/screens/home/feed/disaply_gang_members_page.dart
@@ -39,112 +39,82 @@ class _F_GangMembersState extends State<F_GangMembers> {
     return CustomOfflineWidget(
       onlineChild: Padding(
         padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  colors: [Color(0XffFD8B1F),Colors.white],
-                  begin: Alignment.topCenter, end: Alignment.bottomCenter
-              )
-          ),
-          child: SafeArea(
-            child: Material(
-              child: CustomScrollView(
-                slivers: [
-                  SliverPersistentHeader(
-                    delegate: MySliverAppBar(expandedHeight: getDynamicHeight(200)),
-                    pinned: true,
+        child: Scaffold(
+            body: CustomScrollView(
+              slivers: <Widget>[
+                SliverAppBar(
+                  title: MyAppBar(),
+                  pinned: true,
+                  expandedHeight: 400.0,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: MyFlexiableAppBar(),
                   ),
-                  SliverList(
-                    delegate: SliverChildBuilderDelegate(
-                          (_,index) {
-                            return SingleChildScrollView(
-                              child: Container(
-                                color: Colors.white,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(15.0),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      SizedBox(height: 30,),
-                                      MemberCard("images/boy.png","Vasanthakumar",),
-                                      SizedBox(height: 10,),
-                                      MemberCard("images/girl.png","Srivatsav",),
-                                      SizedBox(height: 10,),
-                                      MemberCard("images/boy.png","VamsiPesala",),
-                                      SizedBox(height: 10,),
-                                      MemberCard("images/girl.png","Rockstar",),
-                                      SizedBox(height: 10,),
-                                      MemberCard("images/boy.png","Nandithaa",),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            );
-                          }
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
+                ),
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    <Widget>[
+                      SingleChildScrollView(
+                        child: Container(
+                          color: Colors.white,
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                SizedBox(height: 10,),
+                                MemberCard("images/boy.png","Vasanthakumar",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/girl.png","Srivatsav",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/boy.png","VamsiPesala",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/girl.png","Rockstar",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/boy.png","Nandithaa",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/boy.png","Vasanthakumar",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/girl.png","Srivatsav",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/boy.png","VamsiPesala",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/girl.png","Rockstar",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/boy.png","Nandithaa",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/boy.png","Vasanthakumar",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/girl.png","Srivatsav",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/boy.png","VamsiPesala",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/girl.png","Rockstar",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/boy.png","Nandithaa",),
+                                SizedBox(height: 30,),
+                                MemberCard("images/boy.png","Vasanthakumar",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/girl.png","Srivatsav",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/boy.png","VamsiPesala",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/girl.png","Rockstar",),
+                                SizedBox(height: 10,),
+                                MemberCard("images/boy.png","Nandithaa",),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ),
+              ],
+            )
         ),
       ),
     );
-  }
-
-  Widget _buildContent(BuildContext context) {
-    return new MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: new Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back_ios,color: Colors.black,),
-              color: Colors.white,
-              onPressed: () {Navigator.pop(context, true);},
-            ),
-            title: GradientText(
-              'Group Members',
-              style: boldStyle,
-              gradient: LinearGradient(
-                colors: [
-                  Color(0XffFD8B1F),
-                  Color(0XffD152E0),
-                  Color(0Xff30D0DB),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-            elevation: 0,
-            backgroundColor: Colors.white,
-          ),
-          body: SingleChildScrollView(
-            child: Container(
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    SizedBox(height: 30,),
-                    MemberCard("images/boy.png","Vasanthakumar",),
-                    SizedBox(height: 10,),
-                    MemberCard("images/girl.png","Srivatsav",),
-                    SizedBox(height: 10,),
-                    MemberCard("images/boy.png","VamsiPesala",),
-                    SizedBox(height: 10,),
-                    MemberCard("images/girl.png","Rockstar",),
-                    SizedBox(height: 10,),
-                    MemberCard("images/boy.png","Nandithaa",),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        )
-    );
-
   }
 
   Widget MemberCard(String imgPath,String name)
@@ -192,7 +162,6 @@ class _F_GangMembersState extends State<F_GangMembers> {
                       ),
                     ],
                   ),
-                  Text("View",style: regularStyle,),
                 ]
             ),
           ),
@@ -210,115 +179,88 @@ class _F_GangMembersState extends State<F_GangMembers> {
 
 }
 
-class MySliverAppBar extends SliverPersistentHeaderDelegate {
-  final double expandedHeight;
+class MyFlexiableAppBar extends StatelessWidget {
 
-  MySliverAppBar({@required this.expandedHeight});
+  final double appBarHeight = 66.0;
+
+  const MyFlexiableAppBar();
 
   @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Stack(
-      fit: StackFit.expand,
-      overflow: Overflow.visible,
-      children: [
-        Container(
-          height: 300,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  colors: [Color(0XffFD8B1F),
-                    Color(0XffD152E0),
-                    Color(0Xff30D0DB),],
-                  begin: Alignment.topCenter, end: Alignment.bottomCenter
-              )
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            IconButton(
-                icon: Icon(
-                  Icons.arrow_back_ios,
-                  color: Colors.white,
-                  size: 30,
-                ),
-                color: Colors.white,
-                onPressed: () {
-                  Navigator.pop(context, true);
-                },
-              ),
+  Widget build(BuildContext context) {
+    final double statusBarHeight = MediaQuery
+        .of(context)
+        .padding
+        .top;
 
+    return new Container(
+      padding: new EdgeInsets.only(top: statusBarHeight),
+      height: statusBarHeight + appBarHeight,
+      child: new Center(
+          child: Column(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      SizedBox(height: getDynamicHeight(30),),
+    Lottie.network("https://assets7.lottiefiles.com/packages/lf20_O2YdXL.json",height: getDynamicHeight(150),width: getDynamicWidth(150)),
+    Text("RockStarts",style: heavyStyle,),
+    Text("6TFBJ73",style: answerStyleBlur,),
+    Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    Column(
+    children: [
+    Icon(Icons.art_track,size: 30,color: Colors.black54,),
+    Text("29",style: boldStyle,)
+    ],
+    ),
+    SizedBox(width: getDynamicWidth(50),),
+    Column(
+    children: [
+    Icon(Icons.account_circle,size: 30,color: Colors.black54,),
+    Text("10",style: boldStyle,)
+    ],
+    )
+    ],
 
-            Center(
-              child: Opacity(
-                opacity: shrinkOffset / expandedHeight,
-                child: Text(
-                    "Group Members",
-                    style: questionStyle1
-                ),
-              ),
-            ),
+    )
+    ],
 
-            Container(),
-
-          ],
-        ),
-        Positioned(
-          top: expandedHeight / 4 - shrinkOffset,
-          left: MediaQuery.of(context).size.width / 12,
-          right: MediaQuery.of(context).size.width / 12,
-          child: Opacity(
-            opacity: (1 - shrinkOffset / expandedHeight),
-            child: Card(
-              elevation: 25,
-              shape: RoundedRectangleBorder(
-                side: BorderSide(color: Colors.black54, width: 2),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Container(
-                height: getDynamicHeight(320),
-                width: getDynamicWidth(500),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Lottie.network("https://assets7.lottiefiles.com/packages/lf20_O2YdXL.json",height: getDynamicHeight(150),width: getDynamicWidth(150)),
-                    Text("RockStarts",style: heavyStyle,),
-                    Text("6TFBJ73",style: answerStyleBlur,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Column(
-                          children: [
-                            Icon(Icons.art_track,size: 30,color: Colors.black54,),
-                            Text("29",style: boldStyle,)
-                          ],
-                        ),
-                        SizedBox(width: getDynamicWidth(50),),
-                        Column(
-                          children: [
-                            Icon(Icons.account_circle,size: 30,color: Colors.black54,),
-                            Text("10",style: boldStyle,)
-                          ],
-                        )
-                      ],
-
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
+    ),
+      ),
+      decoration: new BoxDecoration(
+        color: Colors.white,
+      ),
     );
   }
+}
+
+class MyAppBar extends StatelessWidget {
+
+  final double barHeight = 66.0;
+
+  const MyAppBar();
 
   @override
-  double get maxExtent => expandedHeight;
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          GradientText(
+            'Group Members',
+            style: boldStyle,
+            gradient: LinearGradient(
+              colors: [
+                Color(0XffFD8B1F),
+                Color(0XffD152E0),
+                Color(0Xff30D0DB),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
 
-  @override
-  double get minExtent => kToolbarHeight;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
+        ],
+      ),
+    );
+  }
 }
